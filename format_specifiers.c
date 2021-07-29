f79b813b577eae77da4214e9f3b13341a4524649
mnaibei <muchajulius@gmail.com>
2021-07-29 10:12:36 -0700

    printf correction

--- format_specifiers.c
+++ format_specifiers.c
@@ -1,72 +1,35 @@
 #include "holberton.h"
+
 /**
- * print_char - Prints character
- * @list: list of arguments
- * Return: Will return the amount of characters printed.
+ * get_print_any_func - find print function relative to matching specifier
+ * @c: current specifier to compare
+ *
+ * Return: Number of characters printed
  */
-int print_char(va_list list)
+int (*get_print_any_func(char c))(va_list)
 {
-	_write_char(va_arg(list, int));
-	return (1);
+print_func specs[] = {
+	{'c', print_c},
+	{'s', print_s},
+	{'%', print_percent},
+	{'d', print_d},
+	{'i', print_d},
+	{'b', print_b},
+	{'X', print_X},
+	{'x', print_x},
+	{'o', print_o},
+	{'u', print_u},
+	{'p', print_p},
+	{'S', print_S},
+	{ 0, NULL}
+};
+unsigned int i = 0;
+
+while (specs[i].specifier)
+{
+if (specs[i].specifier == c)
+return (specs[i].f);
+i++;
 }
-
-/**
- * print_string - Prints a string
- * @list: list of arguments
- * Return: Will return the amount of characters printed.
- */
-int print_string(va_list list)
-{
-	int i;
-	char *str;
-
-	str = va_arg(list, char *);
-	if (str == NULL)
-		str = "(null)";
-	for (i = 0; str[i] != '\0'; i++)
-		_write_char(str[i]);
-	return (i);
-}
-
-/**
- * print_percent - Prints a percent symbol
- * @list: list of arguments
- * Return: Will return the amount of characters printed.
- */
-int print_percent(__attribute__((unused))va_list list)
-{
-	_write_char('%');
-	return (1);
-}
-
-/**
- * print_integer - Prints an integer
- * @list: list of arguments
- * Return: Will return the amount of characters printed.
- */
-int print_integer(va_list list)
-{
-	int num_length;
-
-	num_length = print_number(list);
-	return (num_length);
-}
-
-/**
- * unsigned_integer - Prints Unsigned integers
- * @list: List of all of the argumets
- * Return: a count of the numbers
- */
-int unsigned_integer(va_list list)
-{
-	unsigned int num;
-
-	num = va_arg(list, unsigned int);
-
-	if (num == 0)
-		return (print_unsgined_number(num));
-
-	if (num < 1)
-		return (-1);
-	return (print_unsgined_number(num));
+return (NULL);
 }
